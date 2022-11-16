96452bee7004b63996552df1940af2a091432662
Steve Fenton <99181436+steve-fenton-octopus@users.noreply.github.com>
2022-11-16 14:08:47 +0000

    Initial experimental module

--- index.ts
+++ index.ts
@@ -1,2 +1,24 @@
 const x = 'loaded';
 
+type FrontMatter = Record<string, any>;
+type Site = { dateOptions: Intl.DateTimeFormatOptions };
+
+export function formatDate (frontmatter: FrontMatter, lang: string, site: Site) {
+  const date = frontmatter.pubDate ?? '';
+
+  if (date) {
+    return new Date(date).toLocaleDateString(lang, site.dateOptions);
+  }
+  
+  return '';
+}
+
+export function formatModifiedDate (frontmatter: FrontMatter, lang: string, site: Site) {
+  const date = frontmatter.modDate ?? '';
+
+  if (date) {
+    return new Date(date).toLocaleDateString(lang, site.dateOptions);
+  }
+  
+  return '';
+}
