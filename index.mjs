5df8a4e1542aad374888dcf484ace9b210e9b34a
Steve Fenton <github@stevefenton.co.uk>
2022-11-19 17:53:31 +0000

    Switching to MJS files to support both Node and Browsers.

---
+++ index.mjs
@@ -0,0 +1,6 @@
+export { getCachePath, getItem, getItemPath, setItem } from './lib/Cache.mjs';
+export { formatDate, formatModifiedDate } from './lib/dates.mjs';
+export { isAuthor, isListable, isSearch, showInMenu, showInSearch, showInSitemap } from './lib/postFiltering.mjs';
+export { sortByModDate, sortByPubDate, sortByPubDateDesc } from './lib/postOrdering.mjs';
+export { getPages } from './lib/postQueries.mjs';
+export { addSlashToAddress, addSlashToUrl } from './lib/urls.mjs';
