d31db495320367210a68cf16732e6a7d5a95476b
Steve Fenton <github@stevefenton.co.uk>
2022-11-25 12:07:30 +0000

    v0.0.41 Config

--- clean.mjs
+++ clean.mjs
@@ -6,6 +6,7 @@ const workingFolder = process.cwd();
 const files = [
     'index.d.mts',
     '/lib/cache.d.mts',
+    '/lib/config.d.mts',
     '/lib/dates.d.mts',
     '/lib/footerMenu.d.mts',
     '/lib/navigation.d.mts',
---
+++ lib/config.d.mts
@@ -0,0 +1,6 @@
+/**
+ * @typedef { import("../types/Site").Site } Site
+ */
+/** @type {Site} */
+export const SITE: Site;
+export type Site = import("../types/Site").Site;
